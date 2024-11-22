230a680ec821423e500862524c869a03034e1c22
nikkehtine <nikkehtine@int.pl>
2024-11-23 00:17:01 +0100

    Add something

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -10,6 +11,11 @@ const PORT = ":4040"
 
 func main() {
 	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "Hello there"})
+	})
+
 	fmt.Println("Starting server on", PORT)
-	app.Listen(PORT)
+	log.Fatal(app.Listen(PORT))
 }
